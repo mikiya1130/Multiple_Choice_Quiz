7aa1d3c6f4a343fe6ee073f7969ead82f0c178f6
Morisaki Mikiya <git@morisakimikiya.com>
2021-09-04 10:12:48 +0900

    Create quiz layout

--- src/quiz_box.cpp
+++ src/quiz_box.cpp
@@ -1,11 +1,63 @@
 #include "quiz_box.hpp"
 
-QuizBox::QuizBox() : home_button("home")
+QuizBox::QuizBox() : header_box(Gtk::ORIENTATION_HORIZONTAL),
+                     right_side_box(Gtk::ORIENTATION_VERTICAL),
+                     controll_box(Gtk::ORIENTATION_HORIZONTAL),
+                     choice_button_box(Gtk::ORIENTATION_VERTICAL),
+                     question_number_label("○問目"),
+                     statistics_label("正解数/出題数：○問/○問 (正解率：○%)"),
+                     previous_button("←"),
+                     home_button("Home"),
+                     next_button("→"),
+                     choice_label("選択肢")
 {
-    pack_start(home_button);
-    home_button.signal_clicked().connect(
-        [this]
-        { onHomeButtonClicked(); });
+    pack_start(header_box, Gtk::PACK_SHRINK);
+    pack_start(vertical_separator, Gtk::PACK_SHRINK);
+    pack_start(horizontal_paned);
+
+    header_box.pack_start(question_number_label);
+    header_box.pack_start(statistics_label);
+    header_box.set_margin_top(QuizBox::PADDING);
+    header_box.set_margin_right(QuizBox::PADDING * 2);
+    header_box.set_margin_bottom(QuizBox::PADDING);
+    header_box.set_margin_left(QuizBox::PADDING * 2);
+    question_number_label.set_halign(Gtk::Align::ALIGN_START);
+    statistics_label.set_halign(Gtk::Align::ALIGN_END);
+
+    horizontal_paned.pack1(qa_notebook);
+    horizontal_paned.pack2(right_side_box, true, false);
+    horizontal_paned.set_position(9999);
+    horizontal_paned.set_wide_handle(true);
+
+    qa_notebook.append_page(question_box, "問題");
+    qa_notebook.append_page(explanation_box, "解説");
+
+    right_side_box.pack_start(controll_box, Gtk::PACK_SHRINK);
+    right_side_box.pack_start(vertical_right_side_box_separator, Gtk::PACK_SHRINK);
+    right_side_box.pack_start(choice_button_box);
+    right_side_box.set_margin_top(QuizBox::PADDING);
+    right_side_box.set_margin_right(QuizBox::PADDING);
+    right_side_box.set_margin_bottom(QuizBox::PADDING);
+    right_side_box.set_margin_left(QuizBox::PADDING);
+
+    controll_box.pack_start(previous_button, Gtk::PACK_SHRINK);
+    controll_box.pack_start(home_button, Gtk::PACK_SHRINK);
+    controll_box.pack_start(next_button, Gtk::PACK_SHRINK);
+    controll_box.set_spacing(QuizBox::PADDING);
+    controll_box.set_halign(Gtk::Align::ALIGN_CENTER);
+
+    vertical_right_side_box_separator.set_margin_top(QuizBox::PADDING);
+    vertical_right_side_box_separator.set_margin_bottom(QuizBox::PADDING);
+
+    choice_button_box.pack_start(choice_label, Gtk::PACK_SHRINK);
+    choice_button_vector.emplace_back(Gtk::Button("A1"));
+    choice_button_box.pack_start(choice_button_vector.back(), Gtk::PACK_EXPAND_WIDGET, QuizBox::PADDING);
+    choice_button_vector.emplace_back(Gtk::Button("A2"));
+    choice_button_box.pack_start(choice_button_vector.back(), Gtk::PACK_EXPAND_WIDGET, QuizBox::PADDING);
+    choice_button_vector.emplace_back(Gtk::Button("A3"));
+    choice_button_box.pack_start(choice_button_vector.back(), Gtk::PACK_EXPAND_WIDGET, QuizBox::PADDING);
+    choice_button_vector.emplace_back(Gtk::Button("A4"));
+    choice_button_box.pack_start(choice_button_vector.back(), Gtk::PACK_EXPAND_WIDGET, QuizBox::PADDING);
 }
 
 bool QuizBox::onHomeButtonClicked()
--- src/quiz_box.hpp
+++ src/quiz_box.hpp
@@ -7,11 +7,15 @@
 
 #include <gtkmm/box.h>
 #include <gtkmm/button.h>
+#include <gtkmm/label.h>
+#include <gtkmm/separator.h>
+#include <gtkmm/paned.h>
+#include <gtkmm/notebook.h>
 
 /**
  * @brief quiz画面のルートにあたるGtk::Boxの派生クラス
  */
-class QuizBox : public Gtk::HBox
+class QuizBox : public Gtk::VBox
 {
 public:
     /**
@@ -20,7 +24,24 @@ public:
     QuizBox();
 
 private:
+    static const int PADDING = 15;
+    Gtk::Box header_box;
+    Gtk::Label question_number_label;
+    Gtk::Label statistics_label;
+    Gtk::Separator vertical_separator;
+    Gtk::Paned horizontal_paned;
+    Gtk::Notebook qa_notebook;
+    Gtk::Box question_box;
+    Gtk::Box explanation_box;
+    Gtk::Box right_side_box;
+    Gtk::Box controll_box;
+    Gtk::Button previous_button;
     Gtk::Button home_button;
+    Gtk::Button next_button;
+    Gtk::Separator vertical_right_side_box_separator;
+    Gtk::Label choice_label;
+    Gtk::Box choice_button_box;
+    std::vector<Gtk::Button> choice_button_vector;
 
     bool onHomeButtonClicked();
 };
